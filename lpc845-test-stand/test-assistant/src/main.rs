b20ece15d8c177a6ab0bf247f13a64da9cd9283d
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-01-12 16:36:54 +0100

    untangle SetLevel handling a tiny bit

--- lpc845-test-stand/test-assistant/src/main.rs
+++ lpc845-test-stand/test-assistant/src/main.rs
@@ -627,71 +627,43 @@ const APP: () = {
                         ) => {
                             let pin_number = pin.get_pin_number().unwrap();
 
-                            // TODO untangle at least a little bit to reduce pin map operations and
-                            // general mess
-
-                            // todo nicer and more generic once we resolve the Pin Type Conundrum
-                            let pin_is_output: bool = match pin_number {
-                                RED_LED_PIN_NUMBER => pinint0_pin.direction_is_output(),
-                                CTS_PIN_NUMBER => cts.direction_is_output(),
-                                RTS_PIN_NUMBER => rts.direction_is_output(),
+                            // todo less repetitive once we've unified pin types
+                            // TODO maybe just add a set_level() function to the hal?
+                            match pin_number {
+                                RED_LED_PIN_NUMBER => {
+                                    if pinint0_pin.direction_is_output() {
+                                        match level {
+                                            pin::Level::High => { pinint0_pin.set_high() }
+                                            pin::Level::Low => { pinint0_pin.set_low() }
+                                        }
+                                    }
+                                }
+                                CTS_PIN_NUMBER => {
+                                    if cts.direction_is_output() {
+                                        match level {
+                                            pin::Level::High => { cts.set_high() }
+                                            pin::Level::Low => { cts.set_low() }
+                                        }
+                                }}
+                                RTS_PIN_NUMBER => { unreachable!() }
                                 pin_number => {
                                     dyn_noint_pins.lock(|pin_map|{ // TODO turn this into a macro?
                                         if pin_map.contains_key(&pin_number) {
                                             let pin = pin_map.get_mut(&pin_number).unwrap();
-                                            pin.direction_is_output()
+                                            if pin.direction_is_output() {
+                                                match level {
+                                                    pin::Level::High => { pin.set_high() }
+                                                    pin::Level::Low => { pin.set_low() }
+                                                }
+                                            }
                                         }
                                         else {
                                             rprintln!("can't query unsupported pin: {:?}", pin_number);
-                                            false
                                         }
                                     })
-                                },
+                                }
                             };
 
-                            if pin_is_output { // TODO make this an early return
-                                // todo nicer and more generic once we resolve the Pin Type Conundrum
-                                match level {
-                                    pin::Level::High => {
-                                        rprintln!("set {:?} HIGH", pin);
-                                        match pin_number {
-                                            RED_LED_PIN_NUMBER => pinint0_pin.set_high(),
-                                            RTS_PIN_NUMBER => cts.set_high(), // TODO double-check this RTS/CTS mismatch
-                                            pin_number => {
-                                                dyn_noint_pins.lock(|pin_map|{ // TODO turn this into a macro?
-                                                    if pin_map.contains_key(&pin_number) {
-                                                        // this is a dynamic non-interrupt pin, set its level
-                                                        let pin = pin_map.get_mut(&pin_number).unwrap();
-                                                        pin.set_high()
-                                                    }
-                                                })
-                                                // TODO don't fail silently here?
-                                            },
-                                        };
-                                    }
-                                    pin::Level::Low => {
-                                        rprintln!("set {:?} LOW", pin);
-                                        match pin_number {
-                                            RED_LED_PIN_NUMBER => pinint0_pin.set_low(),
-                                            RTS_PIN_NUMBER => cts.set_low(), // TODO double-check this RTS/CTS mismatch
-                                            pin_number => {
-                                                dyn_noint_pins.lock(|pin_map|{ // TODO turn this into a macro?
-                                                    if pin_map.contains_key(&pin_number) {
-                                                        // this is a dynamic non-interrupt pin, set its level
-                                                        let pin = pin_map.get_mut(&pin_number).unwrap();
-                                                        pin.set_low()
-                                                    }
-                                                })
-                                                // TODO don't fail silently here?
-                                            },
-                                        };
-                                    }
-                                }
-                            }
-                            else {
-                                rprintln!("pin #{} is input. Ignoring.",
-                                          pin.get_pin_number().unwrap() );
-                            }
                             Ok(())
                         }
                         HostToAssistant::ReadPin(
